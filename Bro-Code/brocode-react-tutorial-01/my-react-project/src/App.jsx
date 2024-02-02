2a2bdb7e7f1e72efa634eded14a81f84520cc5d4
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-02-01 23:46:57 -0300

    new

---
+++ Bro-Code/brocode-react-tutorial-01/my-react-project/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx";
+import Food from "./Food.jsx";
+
+function App() {
+  return(
+    <>
+    <Header />
+    <Footer />
+    <Food />
+    </> 
+  );
+}
+
+export default App
---
+++ Bro-Code/brocode-react-tutorial-01/my-react-project/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Your website name</p>
+        </footer>
+
+    );
+}
+export default Footer
